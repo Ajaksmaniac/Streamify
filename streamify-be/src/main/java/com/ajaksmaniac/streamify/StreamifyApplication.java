acf9c8b6d239253ce2c4e7d4baad846b3786b0a1
Aleksandar Jovanov <ajakaca@gmail.com>
2023-02-13 10:41:57 +0100

    feat: initial be project

---
+++ streamify-be/src/main/java/com/ajaksmaniac/streamify/StreamifyApplication.java
@@ -0,0 +1,14 @@
+package com.ajaksmaniac.streamify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StreamifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamifyApplication.class, args);
+	}
+
+}
---
+++ streamify-be/src/test/java/com/ajaksmaniac/streamify/StreamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajaksmaniac.streamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
