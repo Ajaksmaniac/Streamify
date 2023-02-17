4b85f621d9590f9a3658625b37abb03d299380be
Aleksandar Jovanov <ajakaca@gmail.com>
2023-02-17 15:59:27 +0100

    feat: fetch video details on homepage

--- streamify-fe/src/components/VideoBox.tsx
+++ streamify-fe/src/components/VideoBox.tsx
@@ -1,16 +1,29 @@
 import { Card, Col, Container } from "react-bootstrap";
-import { VideoBoxProps } from "../constants/types";
+import { Video, VideoBoxProps } from "../constants/types";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+
+
+
 
 export default function VideoBox(props: VideoBoxProps) {
+  const navigate = useNavigate();
+
+  // console.log(video)
   return (
-    <Col xs={6} md={4} lg={2} className="mb-4">
-      <Card>
+    <Col xs={6} md={4} lg={2} className="mb-4"
+    onClick={() => navigate(`/video/${props.video.id}`, { state: { video: props.video } })}>
+       <Card>
         <Card.Body>
-        <h3>{props.video.title}</h3>
-          <p>{props.video.author}</p></Card.Body> 
+        <h3>{props.video.name}</h3>
+        <p>{props.video.postedBy}</p></Card.Body> 
          
         
       </Card>
+      
+
     </Col>
   );
 }
