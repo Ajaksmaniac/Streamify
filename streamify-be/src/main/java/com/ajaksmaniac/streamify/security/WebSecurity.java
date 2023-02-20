642f64d2b19975ded4f7ec9907009493478ec14b
Aleksandar Jovanov <ajakaca@gmail.com>
2023-02-20 12:27:12 +0100

    feat: basic rest auth

--- streamify-be/src/main/java/com/ajaksmaniac/streamify/security/WebSecurity.java
+++ streamify-be/src/main/java/com/ajaksmaniac/streamify/security/WebSecurity.java
@@ -59,7 +59,7 @@ public class WebSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringRequestMatchers("/auth/*"))
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringRequestMatchers("/*"))
                 .cors().disable()
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtToUserConverter))
