bf65b5e1b56e16472e36868d66526e97e4fccb4e
Aleksandar Jovanov <ajakaca@gmail.com>
2023-03-01 14:32:37 +0100

    feat: error handling

--- streamify-be/src/main/java/com/ajaksmaniac/streamify/exception/user/UserNotExistantException.java
+++ streamify-be/src/main/java/com/ajaksmaniac/streamify/exception/user/UserNotExistantException.java
@@ -1,4 +1,4 @@
-package com.ajaksmaniac.streamify.exception;
+package com.ajaksmaniac.streamify.exception.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
