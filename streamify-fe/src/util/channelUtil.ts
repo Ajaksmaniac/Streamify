112a6d68940c4ffc3af978966437398cd265bba2
Aleksandar Jovanov <ajakaca@gmail.com>
2023-05-14 13:50:19 +0200

    feat: delete channel

--- streamify-fe/src/util/channelUtil.ts
+++ streamify-fe/src/util/channelUtil.ts
@@ -12,6 +12,15 @@ export const searchChannelsByKeywords = (keywords: string)=>{
     return axios.get(`${process.env.REACT_APP_BE_SERVER}${process.env.REACT_APP_GET_CHANNEL_SEARCH_ENDPOINT}`,{params:{keywords:keywords}})
 }
 
+export const deleteChannel = (channelId: number|string, accessToken: string)=>{
+    return axios.delete(`${process.env.REACT_APP_BE_SERVER}${process.env.REACT_APP_DELETE_CHANNEL_ENDPOINT}${channelId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "multipart/form-data",
+      },
+    })
+}
+
 export const createChannel = (channelName: String, username: string, token: string)=>{
     console.log(`${process.env.REACT_APP_BE_SERVER}${process.env.REACT_APP_POST_CREATE_CHANNEL_ENDPOINT}`)
     return axios.post(`${process.env.REACT_APP_BE_SERVER}${process.env.REACT_APP_POST_CREATE_CHANNEL_ENDPOINT}`,
