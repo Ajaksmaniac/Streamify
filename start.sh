4f42321948c76e3bbaec8e21b79cffc596f8f9a8
Aleksandar Jovanov <ajakaca@gmail.com>
2023-07-12 11:48:34 +0200

    feat: add docker images and docker compose files

---
+++ start.sh
@@ -0,0 +1,10 @@
+# Running the whole service
+
+docker build ./video-service --tag video-service:latest
+# docker build ./identity-service --tag identity-service:latest
+# docker build ./gateway --tag gateway:latest
+# docker build ./service-discovery --tag discovery-service:latest
+# docker build ./streamify-fe --tag streamify-fe:latest
+
+
+docker-compose up
