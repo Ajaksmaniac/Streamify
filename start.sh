8413af5da3daebf5aa83e7ae170b51516550ff29
Aleksandar Jovanov <ajakaca@gmail.com>
2023-07-24 16:04:44 +0200

    feat: add create unified environment

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ docker build ./video-service --tag video-service:latest
 docker build ./identity-service --tag identity-service:latest
 docker build ./gateway --tag gateway:latest
 docker build ./service-discovery --tag discovery-service:latest
+docker build ./notification-service --tag notification-service:latest
 docker build ./streamify-fe --tag streamify-fe:latest
 
 
